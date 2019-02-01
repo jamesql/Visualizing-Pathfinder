aa8d9d5faad3c6f797d0bcfafe0c637077c819c3
James <42484571+smitteh1@users.noreply.github.com>
2019-02-01 13:25:37 -0500

    Bug Fixes

--- Pathfinder.java
+++ Pathfinder.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -11,7 +12,7 @@ import java.util.TimerTask;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
-public class window extends JFrame implements ActionListener {
+public class Pathfinder extends JFrame implements ActionListener {
 
 	private static String windowName = "Pathfinder";
 	private JButton[][] b = new JButton[25][25]; 
@@ -23,10 +24,10 @@ public class window extends JFrame implements ActionListener {
 	private JButton startButton = new JButton("Start Pathfinder!");
 	
 	public static void main(String[] args) {
-		window m = new window();
+		Pathfinder m = new Pathfinder();
 	}
 	
-	public window() {
+	public Pathfinder() {
 		super(windowName);
 		init();
 	}
@@ -107,20 +108,68 @@ public class window extends JFrame implements ActionListener {
 		double distanceTop =  Math.sqrt(((currentButton[0][1] - endStart[1][1]) * (currentButton[0][1] + 1 - endStart[1][1])) + ((currentButton[0][0] + 1 - endStart[1][0]) * (currentButton[0][0] - endStart[1][0])));
 		double distanceBottom =  Math.sqrt(((currentButton[0][1] - endStart[1][1]) * (currentButton[0][1] + 1 - endStart[1][1])) + ((currentButton[0][0] - 1 - endStart[1][0]) * (currentButton[0][0] - endStart[1][0])));
 		System.out.println("DIS Right : " + distanceRight + "\n" + "DIS Left : " + distanceLeft + "\n" + "DIS Top : " + distanceTop + "\n" + "DIS Bottom : " + distanceBottom + "\n");
-		
-		if (distanceRight <= distanceLeft && distanceRight <= distanceTop && distanceRight <= distanceBottom && !checkWall(currentButton[0][1] + 1, currentButton[0][0]))
-			currentButton[0][1] += 1;
-	else if (distanceTop <= distanceBottom && distanceTop <= distanceLeft && !checkWall(currentButton[0][1], currentButton[0][0] + 1))
-		currentButton[0][0] += 1;
-	else if (distanceBottom <= distanceRight && distanceBottom <= distanceLeft && !checkWall(currentButton[0][1], currentButton[0][0] - 1)) 
-		currentButton[0][0] -= 1;
-	else if (!checkWall(currentButton[0][1] - 1, currentButton[0][0]))
-		currentButton[0][1] -= 1;
+		ArrayList<Double> dis = new ArrayList<Double>();
+		dis.add(distanceRight);
+		dis.add(distanceTop);
+		dis.add(distanceBottom);
+		dis.add(distanceLeft);
+		switch (getLowest(dis, currentButton[0][1], currentButton[0][0])) {
+		case 0:
+			currentButton[0][1]++;
+			break;
+		case 1:
+			currentButton[0][0]++;
+			break;
+		case 2:
+			currentButton[0][0]--;
+			break;
+		case 3:
+			currentButton[0][1]--;
+			break;
+		}
 		
 		System.out.println(currentButton[0][1] + "," + currentButton[0][0]);
 		b[currentButton[0][0]][currentButton[0][1]].setBackground(Color.BLUE);
 				}
 		System.out.println("FINISHED IN : " + (System.currentTimeMillis() - startTime) + "ms!");
+	}
+	
+	public int getLowest(ArrayList<Double> a, int x, int y) {
+		
+		for (int z = 0; z < 10; z++) {
+			switch (a.indexOf(Collections.min(a))) {
+			case 0:
+				if (!checkWall(x + 1, y)){
+					return 0;
+				}else{
+					a.set(0, 999.0);
+				}
+				break;
+			case 1:
+				if (!checkWall(x, y + 1)){
+					return 1;
+				}else{
+					a.set(1, 999.0);
+				}
+				break;
+			case 2:
+				if (!checkWall(x, y - 1)){
+					return 2;
+				}else{
+					a.set(2, 999.0);
+				}
+				break;
+			case 3:
+				if (!checkWall(x - 1, y)){
+					return 3;
+				}else{
+					a.set(3, 999.0);
+				}
+				break;
+			}
+		}
+		return 0;
+
 	}
 	
 	public boolean checkWall(int x, int y) {
